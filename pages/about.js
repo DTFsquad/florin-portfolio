894cc4db6da22e2924ef7c87e5ae08ff045e6fff
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-03-11 22:42:50 +0000

    adding the curly braces on the arrow function for multiple declarations

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,17 @@
-const About = () => (
-    <h1>Hello About Page</h1>
-)
+// functional component - arrow function
+const About = () => {
+    const message = 'Hello World'
+    return (
+        <h1> Hello About Page - {message}</h1>
+    )
+
+}
+
+// functional component - clasic function
+// function About() {
+//     return (
+//         <h1>Hello About Page</h1>
+//     )
+// }
+
 export default About
