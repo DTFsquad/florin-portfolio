4a1bbc22754445e7fca82ec51f6fc488ab6c2701
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-03-11 22:51:52 +0000

    returning to arrow function declaration

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 // functional component - arrow function
-// const About = () => {
-//     const message = 'Hello World'
-//     return (
-//         <h1> Hello About Page - {message}</h1>
-//     )
-// }
-
 const About = () => {
     const message = 'Hello World'
-    return React.createElement('h1', null, 'I am generating this with createElement')
+    return (
+        <h1> Hello About Page - {message}</h1>
+    )
 }
+
+// const About = () => {
+//     const message = 'Hello World'
+//     return React.createElement('h1', null, 'I am generating this with createElement')
+// }
 
 // functional component - clasic function
 // function About() {
